7f06a63bdc2958a136e95f034d427def410dbfcf
dineshsuthar123 <dinesh.suthar18sld@gmail.com>
2025-02-17 22:25:52 +0530

    LeetCode Daily Question(2/17/2025)

---
+++ src/Daily_questions/_1079.java
@@ -0,0 +1,45 @@
+package Daily_questions;
+
+
+public class _1079 {
+    public static void main(String[] args) {
+        Solution_1079 solution = new Solution_1079();
+        String tiles = "AAB"; // Example input
+        System.out.println(solution.numTilePossibilities(tiles));
+    }
+}
+
+class Solution_1079 {
+    public int numTilePossibilities(String tiles) {
+        int[] counts = new int[26];
+        int[] fac = new int[tiles.length() + 1];
+        fac[0] = 1;
+        for (int i = 1; i < fac.length; i++) {
+            fac[i] = i * fac[i - 1];
+        }
+        for (int i = 0; i < tiles.length(); i++) {
+            counts[tiles.charAt(i) - 'A']++;
+        }
+        int[] lengthCounts = new int[tiles.length() + 1];
+        lengthCounts[0] = 1;
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] != 0) {
+                int[] temp = new int[tiles.length() + 1];
+                for (int j = 0; j < lengthCounts.length && lengthCounts[j] > 0; j++) {
+                    for (int k = 1; k <= counts[i]; k++) {
+                        int totalLength = j + k;
+                        temp[totalLength] += lengthCounts[j] * fac[totalLength] / (fac[k] * fac[j]);
+                    }
+                }
+                for (int j = 0; j < temp.length; j++) {
+                    lengthCounts[j] += temp[j];
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i < lengthCounts.length; i++) {
+            ans += lengthCounts[i];
+        }
+        return ans;
+    }
+}
